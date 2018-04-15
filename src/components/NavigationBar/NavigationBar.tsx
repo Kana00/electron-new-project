2a325753dc7ee01ef0ed676d4874ef595670b152
Kana00 <illya.delsarte@gmail.com>
2018-04-15 12:46:35 +0200

    fix some issue with the flexbox

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import Flexbox from 'flexbox-react';
 
 export default class NavigationBar extends React.Component {
   render() {
@@ -7,10 +8,6 @@ export default class NavigationBar extends React.Component {
     const styles = {
       navigationContainer: {
         /* Position Handler  */
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
         position: 'absolute',
         bottom: 0,
         /* Size Handler */
@@ -22,21 +19,18 @@ export default class NavigationBar extends React.Component {
         height: heightOfNavBar + 'rem',
         textAlign: 'center',
         lineHeight: heightOfNavBar + 'rem',
-        fontSize: heightOfNavBar/3 + 'rem',
+        fontSize: heightOfNavBar / 3 + 'rem',
         backgroundColor: '#282C34',
         color: '#BBBBBB',
         textDecorationLine: 'none',
-        hover: {
-          backgroundColor: '#30333b',
-        }
       }
-    }
+    };
     return(
-      <div style={styles.navigationContainer}>
-        <Link style={ styles.styleLink } to={"/"}>Root</Link><br/>
-        <Link style={ styles.styleLink } to={"/home"}>Home</Link><br/>
-        <Link style={ styles.styleLink } to={"/hosdfsdfme"}>NoMatch</Link><br/>
-      </div>
+      <Flexbox flexDirection='row' justifyContent='space-between' alignItems='center' style={styles.navigationContainer}>
+        <Link style={ styles.styleLink } to='/'>Root</Link>
+        <Link style={ styles.styleLink } to='/home'>Home</Link>
+        <Link style={ styles.styleLink } to='/hosdfsdfme'>NoMatch</Link>
+      </Flexbox>
     );
   }
 }
