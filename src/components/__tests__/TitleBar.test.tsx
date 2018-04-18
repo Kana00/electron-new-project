2a3e2b8476d585437b7e248a455a218a5bccbde9
Kana00 <illya.delsarte@gmail.com>
2018-04-18 18:04:14 +0200

    improuve unit test of TitleBar

--- src/components/__tests__/TitleBar.test.tsx
+++ src/components/__tests__/TitleBar.test.tsx
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import TitleBar from '../TitleBar/TitleBar';
 import * as renderer from 'react-test-renderer';
+import * as Adapter from 'enzyme-adapter-react-16';
+import { configure as configureEnzyme } from 'enzyme';
+import { shallow } from 'enzyme';
+
+configureEnzyme({ adapter: new Adapter() });
 
 describe('Test component TitleBar', () => {
   it('render correctly', () => {
     const tree = renderer.create(<TitleBar />).toJSON();
     expect(tree).toMatchSnapshot();
-  })
+  });
+  it('contains WindowsWindowControl', () => {
+    const wrapper = shallow(<TitleBar />);
+    expect(wrapper.find('WindowsWindowControl').length).toBe(1);
+  });
 });
